94f664f4a24a26dce6a8f112018e3e6353b3c36c
alisha027 <119072382+alisha027@users.noreply.github.com>
2024-01-13 22:40:42 +0530

    Update x to the power n.cpp

--- recursion/x to the power n.cpp
+++ recursion/x to the power n.cpp
@@ -26,5 +26,24 @@ Sample Input 2 :
 
 Sample Output 2 :
 32
+ 
+
+ #include<iostream>
+using namespace std;
+
+int power(int x, int n){
+    if(n==0){
+        return 1;
+    }
+    int a = power(x,n-1);
+    return x*a;
+}
+int main(){
+    int x, n;
+    cin >> x >> n;
+  
+    cout << power(x, n) << endl;
+}
+
 
 
